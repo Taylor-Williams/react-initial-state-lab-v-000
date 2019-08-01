d0077a8515f2c59777a144305c470d6923ccd7cc
Taylor Williams <taylor@thewilliams.net>
2019-08-01 10:57:07 +0000

    Automatically backed up by Learn

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -2,7 +2,12 @@
 import React, { Component } from 'react';
 export default class ImageSlider extends Component {
 
-
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentSlideIndex: this.props.value
+    }
+  }
 
   render() {
     return `I am on slide ${{this.state.currentSlideIndex}}`
