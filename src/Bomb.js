de709df0153c1f62278a8b9fd9e42e73dce367a0
Taylor Williams <taylor@thewilliams.net>
2019-08-01 11:01:41 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -2,4 +2,12 @@
 import React, { Component } from 'react';
 export default class Bomb extends Component {
 
+  constructor() {
+    
+  }
+
+  render() {
+    return `${{this.state.secondsLeft}} seconds left before i go boom!`
+  }
+
 }
